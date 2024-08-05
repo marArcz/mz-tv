5534b36e6058bd217d76c216465588f4ae4e8d8b
marArcz <mar19arcz@gmail.com>
2024-08-06 01:09:17 +0800

    auto play video

--- src/_root/pages/Watch.tsx
+++ src/_root/pages/Watch.tsx
@@ -1,19 +1,27 @@
 import ReactPlayer from 'react-player'
 import { Link, useLocation } from 'react-router-dom'
 import toast from 'react-hot-toast';
+import { useState } from 'react';
 
 const Watch = () => {
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
     const stream = searchParams.get('stream');
-
+    const [muted, setMuted] = useState(true)
 
     return (
         <div>
             <Link className=' mb-3' to={'/'}>Back to channel list</Link>
             <br />
             <br />
-            <ReactPlayer onError={() => toast.error("Error: Media cannot be played!")} controls url={stream || 'https://youtu.be/3VHq7MMcs-I?si=niMbvZR0TOO2JcaX'} />
+            <ReactPlayer
+                playing
+                onPlay={() => setMuted(false)}
+                muted={muted}
+                stopOnUnmount
+                onError={() => toast.error("Error: Media cannot be played!")}
+                url={stream || 'https://youtu.be/3VHq7MMcs-I?si=niMbvZR0TOO2JcaX'}
+            />
         </div>
     )
 }
