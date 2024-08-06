b5abe2f6ab1d59507054e4e76ade8b1f95ecef54
marArcz <mar19arcz@gmail.com>
2024-08-06 15:44:21 +0800

    added nba channel

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useChannelStore } from "../store";
 import { phPlaylistUrl } from "../utils";
 
@@ -25,27 +26,24 @@ const Header = () => {
             name: 'Movies',
             url: 'https://iptv-org.github.io/iptv/categories/movies.m3u'
         },
-        {
-            name:'NBA 5',
-            url:'http://112.25.48.68/live/program/live/hdnba5/4000000/mnf.m3u8'
-        },
-        {
-            name:'NBA 7',
-            url:'http://112.25.48.68/live/program/live/hdnba5/4000000/mnf.m3u8'
-        },
-        
     ];
 
     return (
         <header className='py-4 rounded-md border px-4 border-gray-500 flex'>
             <ul className=" list-none flex gap-4">
                 {
-                    categories.map((category,index) => (
-                        <li className="cursor-pointer hover:text-gray-500" key={index} onClick={() => changePlaylist(category.url) }>
+                    categories.map((category, index) => (
+                        <li className="cursor-pointer hover:text-gray-500" key={index} onClick={() => changePlaylist(category.url)}>
                             {category.name}
                         </li>
                     ))
                 }
+                <li className="cursor-pointer hover:text-gray-500">
+                    <Link to={`/watch?stream=https://112.25.48.68/live/program/live/hdnba5/4000000/mnf.m3u8`}>NBA 5</Link>
+                </li>
+                <li className="cursor-pointer hover:text-gray-500">
+                    <Link to={`/watch?stream=https://112.25.48.68/live/program/live/hdnba7/4000000/mnf.m3u8`}>NBA 7</Link>
+                </li>
             </ul>
             <div className="text-center flex-1">
                 <h1 className='font-semibold text-xl'>MZ TV</h1>
